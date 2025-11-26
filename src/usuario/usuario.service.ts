446ac59b4c7a36e21ad423e0a130526660c5cd34
Dominique Campitelli <dominiquecamp@outlook.com>
2025-11-26 14:31:33 -0300

    Criação de usuário no banco de dados, verificação de dados únicos, senha criptografada com bycript, adição da biblioteca bycript

--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 import { PrismaService } from 'src/prisma.service';
@@ -7,8 +8,61 @@ import { PrismaService } from 'src/prisma.service';
 export class UsuarioService {
   constructor(private prisma: PrismaService) {}
 
-  create(createUsuarioDto: CreateUsuarioDto) {
-    return 'This action adds a new usuario';
+  async create(createUsuarioDto: CreateUsuarioDto) {
+    const userAlreadyExists = await this.prisma.user.findUnique({
+      where: {
+        email: createUsuarioDto.email,
+      },
+    });
+
+    if (userAlreadyExists) {
+      throw new UnauthorizedException('User already exists');
+    }
+
+    const cpfAlreadyExists = await this.prisma.user.findUnique({
+      where: { cpf: createUsuarioDto.cpf },
+    });
+
+    if (cpfAlreadyExists) {
+      throw new UnauthorizedException('CPF already registered');
+    }
+
+    const rgAlreadyExists = await this.prisma.user.findUnique({
+      where: { rg: createUsuarioDto.rg },
+    });
+
+    if (rgAlreadyExists) {
+      throw new UnauthorizedException('RG already registered');
+    }
+
+    const cellphoneAlreadyExists = await this.prisma.user.findUnique({
+      where: { cellphone: createUsuarioDto.cellphone },
+    });
+
+    if (cellphoneAlreadyExists) {
+      throw new UnauthorizedException('Cellphone already registered');
+    }
+
+    const hashedPassword = await bcrypt.hash(createUsuarioDto.password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        ...createUsuarioDto,
+        password: hashedPassword,
+      },
+    });
+
+    return {
+      id: user.id,
+      name: user.name,
+      lastName: user.lastName,
+      address: user.address,
+      cpf: user.cpf,
+      rg: user.rg,
+      email: user.email,
+      cellphone: user.cellphone,
+      created_at: user.createdAt,
+    };
   }
 
   async findAll() {
